fbabc335daf57b1a48d31bc6e606e45069d61ad9
cmuhoza <ceddymuhoza@gmail.com>
2022-03-18 12:57:44 -0400

    transactionsByTime middleware

--- src/transactions/Transaction.resolver.ts
+++ src/transactions/Transaction.resolver.ts
@@ -14,9 +14,10 @@ import TransactionModel, {Transaction} from './Transaction.model';
 
 import {ContextType, isAuth} from '@roadmanjs/auth';
 import {isEmpty} from 'lodash';
-import {CouchbaseConnection} from 'couchset';
+import {CouchbaseConnection, getPagination} from 'couchset';
 import {awaitTo} from 'couchset/dist/utils';
 
+const TransactionPagination = getPagination(Transaction);
 @Resolver()
 export class TransactionResolver {
     @Query(() => [Transaction])
@@ -53,6 +54,8 @@ export class TransactionResolver {
         }
     }
 
+    @Query(() => [TransactionPagination])
+    @UseMiddleware(isAuth)
     async transactionsByTime(
         @Ctx() ctx: ContextType,
         @Arg('filter', () => String, {nullable: true}) filter?: string,
