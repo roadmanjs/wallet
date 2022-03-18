91aaf1820b5f815b111fd8f136d96a99a81c3466
cmuhoza <ceddymuhoza@gmail.com>
2022-03-18 12:50:08 -0400

    transactionsByTime

--- src/transactions/Transaction.resolver.ts
+++ src/transactions/Transaction.resolver.ts
@@ -7,11 +7,15 @@ import {
     // Int,
 } from 'type-graphql';
 import _get from 'lodash/get';
+import pickBy from 'lodash/pickBy';
+import identity from 'lodash/identity';
 import {log} from '@roadmanjs/logs';
 import TransactionModel, {Transaction} from './Transaction.model';
 
 import {ContextType, isAuth} from '@roadmanjs/auth';
 import {isEmpty} from 'lodash';
+import {CouchbaseConnection} from 'couchset';
+import {awaitTo} from 'couchset/dist/utils';
 
 @Resolver()
 export class TransactionResolver {
@@ -46,6 +50,79 @@ export class TransactionResolver {
         } catch (error) {
             log('error getting transactions', error);
             return [];
+        }
+    }
+
+    async transactionsByTime(
+        @Ctx() ctx: ContextType,
+        @Arg('filter', () => String, {nullable: true}) filter?: string,
+        @Arg('sort', () => String, {nullable: true}) sortArg?: string,
+        @Arg('before', () => Date, {nullable: true}) before?: Date,
+        @Arg('after', () => Date, {nullable: true}) after?: Date,
+        @Arg('limit', () => Number, {nullable: true}) limitArg?: number
+    ): Promise<{items: Transaction[]; hasNext?: boolean; params?: any}> {
+        const transactionModelName = Transaction.name;
+
+        const owner = _get(ctx, 'payload.userId', '');
+        const bucket = CouchbaseConnection.Instance.bucketName;
+        const sign = before ? '<=' : '>=';
+        const time = new Date(before || after);
+        const sort = sortArg || 'DESC';
+        const limit = limitArg || 10;
+        const limitPassed = limit + 1; // adding +1 for hasNext
+
+        const copyParams = pickBy(
+            {
+                sort,
+                filter,
+                before,
+                after,
+                owner,
+                limit,
+            },
+            identity
+        );
+
+        try {
+            const query = `
+              SELECT *
+                  FROM \`${bucket}\` post
+                  WHERE post._type = "${transactionModelName}"
+                  AND post.owner = "${owner}"
+                  AND post.createdAt ${sign} "${time.toISOString()}"
+                  ORDER BY post.createdAt ${sort}
+                  LIMIT ${limitPassed};
+              `;
+
+            const [errorFetching, data = []] = await awaitTo(
+                TransactionModel.customQuery<any>({
+                    limit: limitPassed,
+                    query,
+                    params: copyParams,
+                })
+            );
+
+            if (errorFetching) {
+                throw errorFetching;
+            }
+
+            const [rows = []] = data;
+
+            const hasNext = rows.length > limit;
+
+            if (hasNext) {
+                rows.pop(); // remove last element
+            }
+
+            const dataToSend = rows.map((d) => {
+                const {post} = d;
+                return TransactionModel.parse(post);
+            });
+
+            return {items: dataToSend, params: copyParams, hasNext};
+        } catch (error) {
+            log('error getting transactions', error);
+            return {items: [], hasNext: false, params: copyParams};
         }
     }
 }
