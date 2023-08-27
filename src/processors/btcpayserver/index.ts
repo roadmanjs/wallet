ef7b0c2f699c8c2229e66026add0de461ed5a853
ceddybi <ceddymuhoza@gmail.com>
2023-08-27 17:44:07 -0400

    btcpayserver roadman

---
+++ src/processors/btcpayserver/index.ts
@@ -0,0 +1,29 @@
+import {btcpayServerStore, btcpayServerToken, btcpayServerUrl} from './btcpayserver.config';
+
+import {RoadmanBuild} from '@roadmanjs/core';
+import {isEmpty} from 'lodash';
+import {log} from '@roadmanjs/logs';
+import {startBtcpayserverPullingCron} from './btcpayserver.cron';
+
+export const btcpayserverRoadman = async (args: RoadmanBuild): Promise<RoadmanBuild> => {
+    // TODO verify enabled providers
+    // if btcpayserver, start pulling queues
+    if (!isEmpty(btcpayServerToken) && !isEmpty(btcpayServerStore) && !isEmpty(btcpayServerUrl)) {
+        log(`btcpayserver enabled at ${btcpayServerUrl}`);
+        startBtcpayserverPullingCron();
+    } else {
+        log(
+            `btcpayserver requires BTCPAYSERVER_TOKEN, BTCPAYSERVER_STORE, BTCPAYSERVER_URL and or BTCPAYSERVER_BTC, BTCPAYSERVER_XMR`
+        );
+    }
+
+    // ...other providers
+
+    return args;
+};
+
+export * from './blockchain';
+export * from './btcpayserver';
+export * from './btcpayserver.config';
+export * from './btcpayserver.queue';
+export * from './btcpayserver.cron';
--- src/processors/index.ts
+++ src/processors/index.ts
@@ -1,2 +1,3 @@
 export * from './stripe';
 export * from './nowpayments';
+export * from './btcpayserver';
