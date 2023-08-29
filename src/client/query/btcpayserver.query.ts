9b8ca98628d9ce33b6042144cdd05a547692ad33
ceddybi <ceddymuhoza@gmail.com>
2023-08-28 21:00:35 -0400

    BtcpayserverRate client

--- src/client/query/btcpayserver.query.ts
+++ src/client/query/btcpayserver.query.ts
@@ -1,5 +1,10 @@
 import gql from 'graphql-tag';
 
+export interface BtcpayserverRate {
+    currencyPair: string;
+    rate: number;
+}
+
 export const FETCH_RATES_QUERY = gql`
     query FetchRates($currencyPair: String!) {
         data: fetchRates(currencyPair: $currencyPair) {
