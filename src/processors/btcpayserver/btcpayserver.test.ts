3e23f18ecc750a942e96960abec253c510269669
ceddybi <ceddymuhoza@gmail.com>
2023-08-30 00:44:07 -0400

    createTransactions fullfill withdraw

--- src/processors/btcpayserver/btcpayserver.test.ts
+++ src/processors/btcpayserver/btcpayserver.test.ts
@@ -2,10 +2,12 @@ import 'mocha';
 import "dotenv/config";
 import "reflect-metadata"
 
-import { fetchRates, fetchStatus, fetchStores, fetchTransactions, generateAddress } from "./btcpayserver";
+import { createTransactions, fetchRates, fetchStatus, fetchStores, fetchTransactions, generateAddress } from "./btcpayserver";
 
 import { expect } from 'chai';
 import { getTxAddressFromBlockchain } from "./blockchain";
+
+const externalAddress = "399KPGqPiNqRygRtf6SYQUb5YHRH5BeWiz";
 
 describe('Btcpayserver', () => {
     // it('it should fetch all stores', async () => {
@@ -14,11 +16,11 @@ describe('Btcpayserver', () => {
     //     expect(stores).to.be.not.null;
     // });
 
-    it('it should fetch rates', async () => {
-        const rates = await fetchRates("BTC_USD,EUR_BTC");
-        console.log('all stores', rates);
-        expect(rates).to.be.not.null;
-    });
+    // it('it should fetch rates', async () => {
+    //     const rates = await fetchRates("BTC_USD,EUR_BTC");
+    //     console.log('all stores', rates);
+    //     expect(rates).to.be.not.null;
+    // });
 
     // it('it should create an address', async () => {
     //     const address = await generateAddress('BTC');
@@ -32,10 +34,28 @@ describe('Btcpayserver', () => {
     //     expect(transactions).to.be.not.null;
     // });
 
+
+    // 
+
+    it('it should get transaction address from blockchain', async () => {
+        const transactionAddress = await getTxAddressFromBlockchain('84e6623c36b2f1f9b38fbf3ed663efa5c1f172c6c3f1d32e4aa69fb2a77b6987');
+        console.log('transactionAddress', transactionAddress);
+        expect(transactionAddress.map(tx => tx.address)).to.include(externalAddress)
+    });
+
     // it('it should get transaction address from blockchain', async () => {
     //     const transactionAddress = await getTxAddressFromBlockchain('a29d731ae7619d1194787d95b82b655c9f3e35d792ae23eeeefb07d5e8e7d638');
     //     console.log('transactionAddress', transactionAddress);
     //     expect(transactionAddress.map(tx => tx.address)).to.include("bc1qn3jmr0tsujhdw753vvptudaeuasval25snuytc")
+    // });
+
+    // it('it should create a transactions', async () => {
+    //     const transactions = await createTransactions('btc', [
+    //         { amount: "0.0006131", destination: externalAddress, subtractFromAmount: true }
+    //     ]);
+
+    //     console.log('created transaction', transactions);
+    //     expect(transactions).to.be.not.null;
     // });
 
 
---
+++ src/wallet/withdrawRequest.model.ts
@@ -0,0 +1,96 @@
+import {Field, InputType, Model, ObjectType, getPagination} from 'couchset';
+import {UserType} from '@roadmanjs/auth';
+
+import {Wallet} from './Wallet.model';
+
+export const WithdrawRequestModelName = 'WithdrawRequest';
+
+export const WithdrawRequestStatus = {
+    requested: 'requested',
+    accepted: 'accepted',
+    cancelled: 'cancelled',
+    completed: 'completed',
+};
+/**
+ * GraphQL Types start
+ */
+@InputType('WithdrawRequestInput')
+@ObjectType()
+export class WithdrawRequest {
+    // Automatic
+    @Field(() => String, {nullable: true})
+    id?: string = '';
+
+    @Field(() => String, {nullable: true})
+    owner?: string | UserType = '';
+
+    @Field(() => Date, {nullable: true})
+    createdAt?: Date = new Date();
+
+    @Field(() => Date, {nullable: true})
+    updatedAt?: Date = new Date();
+
+    @Field(() => Boolean, {nullable: true})
+    deleted?: boolean = false;
+    // Automatic
+
+    @Field({nullable: true, description: 'Type of withdraw request crypto | bank | xxxx'})
+    type?: string = '';
+
+    @Field({nullable: true})
+    receiver?: string = '';
+
+    @Field({nullable: true, description: 'Transaction hash if a crypto withdraw request'})
+    transactionHash?: string = '';
+
+    @Field({nullable: true})
+    status?: string = '';
+
+    // if status === cancelled
+    @Field({nullable: true})
+    reason?: string = '';
+
+    @Field({nullable: true})
+    currency?: string = '';
+
+    @Field({nullable: true})
+    amount?: number = 0;
+
+    @Field({nullable: true, description: 'Wallet Id'})
+    walletId?: string = ''; // no auto
+}
+
+@ObjectType()
+export class WithdrawRequestOutput extends WithdrawRequest {
+    @Field({nullable: true, description: 'Owner'})
+    owner?: UserType; // no auto
+
+    @Field({nullable: true, description: 'Wallet'})
+    wallet?: Wallet; // no auto
+}
+
+export const WithdrawRequestPagination = getPagination(WithdrawRequest);
+export const WithdrawRequestOutputPagination = getPagination(WithdrawRequestOutput);
+
+export const orderSelectors = [
+    'id',
+    'owner',
+    'createdAt',
+    'updatedAt',
+    'type',
+    'receiver',
+    'transactionHash',
+    'status',
+    'reason',
+    'currency',
+    'amount',
+    'walletId',
+];
+
+export const WithdrawRequestModel: Model = new Model(WithdrawRequestModelName, {
+    graphqlType: WithdrawRequest,
+});
+
+export default WithdrawRequestModel;
+
+export {resolver as WithdrawRequestDefaultResolver};
