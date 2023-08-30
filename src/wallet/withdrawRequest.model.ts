50a3236d2cae4fc286ee8dd1e1620ce6485d72f7
ceddybi <ceddymuhoza@gmail.com>
2023-08-30 00:56:37 -0400

    add fields types

--- src/wallet/withdrawRequest.model.ts
+++ src/wallet/withdrawRequest.model.ts
@@ -1,4 +1,4 @@
-import {Field, InputType, Model, ObjectType, getPagination} from 'couchset';
+import {Field, InputType, Model, ObjectType} from 'couchset';
 
 import {UserType} from '@roadmanjs/auth';
 import {Wallet} from './Wallet.model';
@@ -34,43 +34,46 @@ export class WithdrawRequest {
     deleted?: boolean = false;
     // Automatic
 
-    @Field({nullable: true, description: 'Type of withdraw request crypto | bank | xxxx'})
+    @Field(() => String, {
+        nullable: true,
+        description: 'Type of withdraw request crypto | bank | xxxx',
+    })
     type?: string = '';
 
-    @Field({nullable: true})
+    @Field(() => String, {nullable: true})
     receiver?: string = '';
 
-    @Field({nullable: true, description: 'Transaction hash if a crypto withdraw request'})
+    @Field(() => String, {
+        nullable: true,
+        description: 'Transaction hash if a crypto withdraw request',
+    })
     transactionHash?: string = '';
 
-    @Field({nullable: true})
+    @Field(() => String, {nullable: true})
     status?: string = '';
 
     // if status === cancelled
-    @Field({nullable: true})
+    @Field(() => String, {nullable: true})
     reason?: string = '';
 
-    @Field({nullable: true})
+    @Field(() => String, {nullable: true})
     currency?: string = '';
 
-    @Field({nullable: true})
+    @Field(() => Number, {nullable: true})
     amount?: number = 0;
 
-    @Field({nullable: true, description: 'Wallet Id'})
+    @Field(() => String, {nullable: true, description: 'Wallet Id'})
     walletId?: string = ''; // no auto
 }
 
 @ObjectType()
 export class WithdrawRequestOutput extends WithdrawRequest {
-    @Field({nullable: true, description: 'Owner'})
+    @Field(() => UserType, {nullable: true, description: 'Owner'})
     owner?: UserType; // no auto
 
-    @Field({nullable: true, description: 'Wallet'})
+    @Field(() => Wallet, {nullable: true, description: 'Wallet'})
     wallet?: Wallet; // no auto
 }
-
-export const WithdrawRequestPagination = getPagination(WithdrawRequest);
-export const WithdrawRequestOutputPagination = getPagination(WithdrawRequestOutput);
 
 export const orderSelectors = [
     'id',
