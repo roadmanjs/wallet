788a71033427e0f15c2d0d6aa31529a626ff75d1
ceddybi <ceddymuhoza@gmail.com>
2023-10-18 19:04:13 -0400

    resType

--- src/client/query/wallet.mutation.ts
+++ src/client/query/wallet.mutation.ts
@@ -1,11 +1,11 @@
-import {ResTypeFragment} from 'couchset';
+import {WalletResTypeFragment} from './resType';
 import gql from 'graphql-tag';
 
 export const GENERATE_WALLET_ADDRESS_MUTATION = gql`
     mutation GetWalletAddress($currency: String!, $forceGenerate: Boolean) {
         data: getWalletAddress(currency: $currency, forceGenerate: $forceGenerate) {
-            ...ResTypeFragment
+            ...WalletResTypeFragment
         }
     }
-    ${ResTypeFragment}
+    ${WalletResTypeFragment}
 `;
